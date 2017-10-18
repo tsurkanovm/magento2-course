2a9b25f89e24c7279442194a03e2d9e9c788218c
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-10-18 15:43:31 +0300

    add new test module and rewrite path observer

---
+++ app/code/Training/Test/Observer/RedirectToProductView.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Training\Test\Observer;
+
+use Magento\Framework\App\ActionFlag;
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\UrlInterface;
+use Psr\Log\LoggerInterface;
+
+class RedirectToProductView implements ObserverInterface
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $_logger;
+
+    /**
+     * @var UrlInterface
+     */
+    protected $_urlInterFace;
+
+    /**
+     * @var ActionFlag
+     */
+    protected $_actionFlag;
+
+    /**
+     * @param ActionFlag $_actionFlag
+     * @param UrlInterface $_urlInterFace
+     * @param LoggerInterface $_logger
+     */
+    public function __construct(
+        ActionFlag $_actionFlag,
+        UrlInterface $_urlInterFace,
+        LoggerInterface $_logger
+    )
+    {
+        $this->_actionFlag = $_actionFlag;
+        $this->_urlInterFace = $_urlInterFace;
+        $this->_logger = $_logger;
+    }
+
+    public function execute(Observer $observer)
+    {
+        $request = $observer->getEvent()->getData('request');
+
+        if (($request->getModuleName() == 'catalog' || $request->getModuleName() == 'review')
+            & $request->getControllerName() == 'product') {
+            return;
+        }
+
+        $url = $this->_urlInterFace->getUrl('catalog/product/view/id/1');
+        $this->_logger->debug('Url->' . $url);
+        $action = $observer->getEvent()->getControllerAction();
+        $this->_actionFlag->set('', ActionInterface::FLAG_NO_DISPATCH, true);
+        $action->getResponse()->setRedirect($url);
+    }
+}
---
+++ app/code/Training/Test/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Training_Test',
+    __DIR__
+);
