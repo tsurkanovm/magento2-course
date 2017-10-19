fc7b3bec66551991e41848c0dde93e42e3f844ba
Mihail Tsurkanov <mtsurkanov@grossum.com>
2017-10-19 15:09:05 +0300

    delete comments from CustomRouter

--- app/code/Training/Test/App/Router/CustomRouter.php
+++ app/code/Training/Test/App/Router/CustomRouter.php
@@ -27,12 +27,7 @@ class CustomRouter implements RouterInterface
         if (preg_match("%^/(.*?)-(.*?)-(.*?)$%", $info, $m)) {
             $request->setPathInfo(sprintf("/%s/%s/%s", $m[1], $m[2], $m[3]));
 
-
             return $this->actionFactory->create(\Magento\Framework\App\Action\Forward::class);
-
-
-//            return $this->actionFactory->create('Magento\Framework\App\Action\Forward',
-//                ['request' => $request]);
         }
         return null;
     }
