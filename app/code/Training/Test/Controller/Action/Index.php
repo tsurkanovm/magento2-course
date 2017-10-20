c6ae7b515c94e47554aec81662d0f1c48305791b
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-10-20 08:45:23 +0300

    add redirect to catalog

--- app/code/Training/Test/Controller/Action/Index.php
+++ app/code/Training/Test/Controller/Action/Index.php
@@ -35,6 +35,7 @@ class Index extends Action
      */
     public function execute()
     {
-        $this->getResponse()->appendBody("HELLO WORLD");
+//        $this->getResponse()->appendBody("HELLO WORLD");
+        $this->_redirect('catalog/category/view/id/1');
     }
 }
