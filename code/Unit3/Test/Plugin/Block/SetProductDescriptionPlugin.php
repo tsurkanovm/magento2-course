b1e52dbc6e9ccf73b7e54468c08c85553bac0c8c
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-10-25 16:12:42 +0300

    create Unit3_Test2 module and add ProductTemplateDescriptionPlugin

--- code/Unit3/Test/Plugin/Block/SetProductDescriptionPlugin.php
+++ code/Unit3/Test/Plugin/Block/SetProductDescriptionPlugin.php
@@ -4,9 +4,9 @@ namespace Unit3\Test\Plugin\Block;
 
 use Magento\Catalog\Block\Product\View\Description;
 
-class ProductViewDescriptionPlugin
+class SetProductDescriptionPlugin
 {
     public function beforeToHtml(Description $originalBlock) {
-        $originalBlock->getProduct()->setDescription('Test description');
+        $originalBlock->getProduct()->setDescription('New!! Test description');
     }
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 ## Module 3. Rendering Flow
-### 3.6.2. Create a template block and a custom template file for it. Render the block in the controller.
+### 3.6.3. Customize the Catalog\Block\Product\View\Description block and assign a custom template to it.
