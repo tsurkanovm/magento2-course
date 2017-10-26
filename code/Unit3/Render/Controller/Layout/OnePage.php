7cb25e9052e8b3d9f93b6df357032931b1279051
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-10-26 11:12:10 +0300

    create Unit3_Render module and add default layout and OnePage action

---
+++ code/Unit3/Render/Controller/Layout/OnePage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Unit3\Render\Controller\Layout;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Action;
+
+/**
+ * Class Index
+ * @package Unit3\Render\Controller\Layout\OnePage
+ */
+class OnePage extends Action
+{
+    /**
+     * Index resultPageFactory
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        return parent::__construct($context);
+    }
+
+    /**
+     * Function execute
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,10 @@
 ## Module 3. Rendering Flow
-### 3.6.3. Customize the Catalog\Block\Product\View\Description block and assign a custom template to it.
+### 3.8.1. Add a default.xml layout file to the Training_Render module.
+    1.Reference the content.top container.
+    2.Add a Magento\Framework\View\Element\Template block with a custom template.
+    3.Create your custom template.
+    4.Check that the template content is visible on every page.
+    
+### 3.8.2. Create a new controller action (ex: training_render/layout/onepage).
+    1. For that action, choose a single-column page layout using layout XML.
+    2. Set a page title using layout XML.
