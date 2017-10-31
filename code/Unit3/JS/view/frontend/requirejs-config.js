86a0cccbc3ae90de528954d4123b01d77b992815
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-10-31 07:29:46 +0200

    add Unit3_JS module with attribute_counter script.

---
+++ code/Unit3/JS/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            attributesCounter: 'Unit3_JS/js/attribute_counter'
+        }
+    }
+};
---
+++ code/Unit3/JS/view/frontend/web/js/attribute_counter.js
@@ -0,0 +1,5 @@
+define(function () {
+   return function (config) {
+       console.log('There is a ' + config.count + ' attributes');
+   }
+});
