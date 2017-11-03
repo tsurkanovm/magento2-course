4f00cdddf5d5960fe848dc7b885522acb9d6a202
Tsurkanov Mikhail <tsurkanovm@gmail.com>
2017-11-03 09:44:37 +0200

    merge with JS3 and JS4

--- code/Unit3/JS/view/frontend/requirejs-config.js
+++ code/Unit3/JS/view/frontend/requirejs-config.js
@@ -2,7 +2,8 @@ var config = {
     map: {
         '*': {
             attributesCounter: 'Unit3_JS/js/attribute_counter',
-            actionNameAlert:   'Unit3_JS/js/action_name_alert'
+            actionNameAlert:   'Unit3_JS/js/action_name_alert',
+            formDumper:        'Unit3_JS/js/form_dumper'
         }
     }
 };
